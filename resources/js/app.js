5bef61aad41f05458c7c6f5845cb211d8d18cb4f
enrishtjefni <enri.shtjefni@ash.al>
2024-12-03 14:58:18 +0100

    to do management

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue'
+import '../../vue/src/style.css'
+import App from '../../vue/src/App.vue'
+import ToDo from "../../vue/src/components/ToDo.vue";
+
+createApp(App).component('ToDo', ToDo).mount('#app')
